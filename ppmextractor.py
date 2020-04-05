84d2b98cf1e8d6b96686a802bf1aef6b3ffe76a7
denbergvanthijs <denbergvanthijs@gmail.com>
2020-04-05 15:45:41 +0200

    Added typehints and option to filter extensions

--- ppmextractor.py
+++ ppmextractor.py
@@ -3,15 +3,18 @@ import shutil
 import tempfile
 import zipfile
 from os import path
+from typing import Tuple
 
 
-def ppmextractor(fp: str, fp_out: str = os.getcwd() + "/media") -> str:
+def ppmextractor(fp: str, fp_out: str = os.getcwd() + "/media", filter_ext: Tuple[str] = []) -> str:
     """Main function.
 
     :param fp: Filepath of the Powerpoint-file.
     :type  fp: str
     :param fp_out: Filepath of the output-folder
     :type  fp_out: str, defaults to `os.getcwd() + '/media'`
+    :param filter_ext: Tuple of file-extensions to extract
+    :type  filter_ext: Tuple[str], defaults to `[]`
 
     :returns: Filepath to the folder of the extracted files
     :rtype: str
@@ -30,9 +33,13 @@ def ppmextractor(fp: str, fp_out: str = os.getcwd() + "/media") -> str:
             files = archive.namelist()
             assert "ppt/media" in {path.dirname(f) for f in files}, "Zip-file does not contain an media-folder"
 
-            for f in files:  # List al files
-                if f.startswith("ppt/media"):  # Extract only files in the media-folder
-                    archive.extract(f, tmp_dir)
+            for f in files:  # List of all files
+                if f.startswith("ppt/media"):  # If file is in media folder
+                    if filter_ext:
+                        if f.endswith(filter_ext):  # If file-extention in `filter_ext`
+                            archive.extract(f, tmp_dir)
+                    else:
+                        archive.extract(f, tmp_dir)
 
         tmp_media_dir = tmp_dir + "/ppt/media"
         shutil.copytree(tmp_media_dir, fp_out)
@@ -42,6 +49,6 @@ def ppmextractor(fp: str, fp_out: str = os.getcwd() + "/media") -> str:
 
 if __name__ == "__main__":
     # output = ppmextractor("./data/test_old.ppt")
-    output = ppmextractor("./data/test_s.ppsx")
-    # output = ppmextractor("./data/test_x.pptx")
-    # print(output)
+    # output = ppmextractor("./data/test_s.ppsx", filter_ext=(".png", ".jpeg"))
+    output = ppmextractor("./data/test_x.pptx", filter_ext=(".m4a"))
+    print(output)
